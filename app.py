39887b4e8118dfe8aa51441a87ef6735d871057e
Carlos Moraes <cmoraes@unifesp.br>
2023-02-09 17:56:37 -0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from service.modeling import Modeling
+from flask_cors import CORS
+from flask_cors import cross_origin
+import os
+
+app = Flask(__name__)
+ucs = Modeling()
+CORS(app)
+
+class Grade():
+
+    @app.route("/disciplinas", methods=['GET'])
+    def get():
+        return ucs.get_ucs()
+
+    @app.route("/disciplinas", methods=['POST'])
+    def post():
+        data = request.get_json()
+        data = data['items']
+        return ucs.uc_analizer(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
