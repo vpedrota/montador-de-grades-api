e75767430327ce2ae34848781fe09e0d63144d30
Carlos Moraes <cmoraes@unifesp.br>
2023-02-10 12:38:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,29 +7,29 @@ import os
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-CORS(app, resources={r"/disciplinas": {"origins": "*"},
-                    r"/prof": {"origins": "*"}})
+CORS(app, resources={r"/disciplinas": {"origins": "https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080"},
+                    r"/prof": {"origins": "https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080"}})
 ucs = Modeling()
 
 class Grade():
 
     @app.route("/disciplinas", methods=['GET'])
-    @cross_origin(origin='*',headers=['Content- Type','Authorization'])
+    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080',headers=['Content- Type','Authorization'])
     def get():
         return ucs.get_ucs()
 
     @app.route("/disciplinas", methods=['POST'])
-    @cross_origin(origin='*',headers=['Content- Type','Authorization'])
+    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080',headers=['Content- Type','Authorization'])
     def post_uc():
         data = request.get_json()
         return ucs.uc_analizer(data['items'])
 
     @app.route("/prof", methods=['POST'])
-    @cross_origin(origin='*',headers=['Content- Type','Authorization'])
+    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080',headers=['Content- Type','Authorization'])
     def post_prof():
         data = request.get_json()
         return ucs.prof_analizer(data['items'])
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
