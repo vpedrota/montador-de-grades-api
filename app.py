289efb28d59b19609bea4ae86d1bfe884fe47f01
Carlos Moraes <cmoraes@unifesp.br>
2023-02-10 12:44:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,25 +7,25 @@ import os
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-CORS(app, resources={r"/disciplinas": {"origins": "https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080"},
-                    r"/prof": {"origins": "https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080"}})
+CORS(app, resources={r"/disciplinas": {"origins": "https://montador-de-grades-api-upfpc35ezq-uc.a.run.app"},
+                    r"/prof": {"origins": "https://montador-de-grades-api-upfpc35ezq-uc.a.run.app"}})
 ucs = Modeling()
 
 class Grade():
 
     @app.route("/disciplinas", methods=['GET'])
-    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080',headers=['Content- Type','Authorization'])
+    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app',headers=['Content- Type','Authorization'])
     def get():
         return ucs.get_ucs()
 
     @app.route("/disciplinas", methods=['POST'])
-    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080',headers=['Content- Type','Authorization'])
+    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app',headers=['Content- Type','Authorization'])
     def post_uc():
         data = request.get_json()
         return ucs.uc_analizer(data['items'])
 
     @app.route("/prof", methods=['POST'])
-    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app:8080',headers=['Content- Type','Authorization'])
+    @cross_origin(origin='https://montador-de-grades-api-upfpc35ezq-uc.a.run.app',headers=['Content- Type','Authorization'])
     def post_prof():
         data = request.get_json()
         return ucs.prof_analizer(data['items'])
