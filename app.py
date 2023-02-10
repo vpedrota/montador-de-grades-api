9eba9ff7e64750ae3c4d773ba6c181030dcd1b0d
Carlos Moraes <cmoraes@unifesp.br>
2023-02-10 01:45:13 -0300

    Update Feature

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ class Grade():
         data = request.get_json()
         return ucs.prof_analizer(data['items'])
 
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
